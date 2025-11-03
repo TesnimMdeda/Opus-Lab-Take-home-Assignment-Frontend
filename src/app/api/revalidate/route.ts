356cde4585e53e16532f70161d36382a5a270f96
tasnim <tasnim@dieture.com>
2025-11-03 15:03:03 +0100

    fix .enc url

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get("secret");
 
-  if (secret !== process.env.REVALIDATE_SECRET) {
+  if (secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
